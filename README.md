15d6ccba503957c1620bcd6db0796cd0bc950c29
Abdulgee773 <abdullahihudu773@gmail.com>
2022-02-21 22:45:34 +0100

    abdulgee

---
+++ README.md
@@ -0,0 +1,22 @@
+
+### Termux Commands
+
+
+* `apt update`
+
+* `apt upgrade`
+
+* `pkg install python`
+
+* `pkg install python2`
+
+* `pip install mechanize`
+
+* `pkg install git`
+
+* https://github.com/Abdulgee773/yazeed-abbalo.git
+
+* `cd 
+
+* `python
+
---
+++ abdulgee.py
@@ -0,0 +1,285 @@
+import random
+import time
+import pyfiglet
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
+result = pyfiglet.figlet_format("GEE", font = "doh")
+print(result)
+print("""
++=========================================+
+|..........   PREMIER HACKERS   ...........|
+              ▇▇▇▇▇▇▇▇▇▇▇▇
+Author: ABDULGEE 
+
+forum: HAUSA HACKERS/HAUSA PROGRAMNERS 
+
+contact: +2348131116661
+
+fb_account: itz real Abdul gee
+
+☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠
+
+_______________HAUSA PROGRAMMERS_________________
+::::::::::----------TOOL----------::::::::::
+""")
+a = ""
+print("1. hack facebook account")
+print("2. hack IG account")
+print("3. hack tiktok account")
+print("4. bypass OTP code")
+print("""5. hack whatsapp account
+
+""")
+
+
+a = input("select option:  ").lower()
+if a == "1":
+	os.system('cls' if os.name == 'nt' else 'clear')
+	print(""" +=========================================+
+|..........   PREMIER HACKERS   ...........|
+              ▇▇▇▇▇▇▇▇▇▇▇▇
+Author: ABDULGEE 
+
+forum: HAUSA HACKERS/HAUSA PROGRAMNERS 
+
+contact: +2348131116661
+
+fb_account: itz real Abdul gee
+
+☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠
+         (҂`_´)
+         <,︻╦̵̵̿╤─ ҉     ~  • ABDULGEE
+█۞███████]▄▄▄▄▄▄▄▄▄▄▃ ●●●
+▂▄▅█████████▅▄▃▂…
+[███████████████████]
+◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙	
+h̷a̷c̷k̷ F̷a̷c̷e̷b̷o̷o̷k̷ a̷c̷c̷o̷u̷n̷t̷====h̷a̷c̷k̷ F̷a̷c̷e̷b̷o̷o̷k̷ a̷c̷c̷o̷u̷n̷t̷
+h̷a̷c̷k̷ F̷a̷c̷e̷b̷o̷o̷k̷ a̷c̷c̷o̷u̷n̷t̷====h̷a̷c̷k̷ F̷a̷c̷e̷b̷o̷o̷k̷ a̷c̷c̷o̷u̷n̷t̷
+h̷a̷c̷k̷ F̷a̷c̷e̷b̷o̷o̷k̷ a̷c̷c̷o̷u̷n̷t̷====h̷a̷c̷k̷ F̷a̷c̷e̷b̷o̷o̷k̷ a̷c̷c̷o̷u̷n̷t̷
+""")
+	a = input("""
+Target username/id: """)
+	time.sleep(5)
+	print("CRACKING......please wait......? 10% ")
+	time.sleep(3)
+	print("CRACKING......please wait......? 20% ")
+	time.sleep(3)
+	print("CRACKING......please wait......? 30% ")
+	time.sleep(3)
+	print("CRACKING......please wait......? 40% ")
+	time.sleep(3)
+	print("CRACKING......please wait......? 50% ")
+	time.sleep(3)
+	print("CRACKING......please wait......? 60%")
+	time.sleep(3)
+	print("CRACKING......please wait......? 70% ")
+	time.sleep(3)
+	print("CRACKING......please wait......? 80% ")
+	time.sleep(3)
+	print("CRACKING......please wait......? 90%")
+	time.sleep(3)
+	print("CRACKING......please wait......? 99%")
+	time.sleep(3)
+	print("CRACKING......please wait......? 100%")
+	time.sleep(5)
+	print("""
+	
+	HACKED SUCCESSFUL
+	
+	
+	""")
+	print("id/username is ", a)
+	print("password is ==> ", random.randint(1111111, 999999999999))
+############ facebook account done#######
+
+elif a == "2":
+	os.system('cls' if os.name == 'nt' else 'clear')
+	print("""
+	+=========================================+
+|..........   PREMIER HACKERS   ...........|
+              ▇▇▇▇▇▇▇▇▇▇▇▇
+Author: ABDULGEE 
+
+forum: HAUSA HACKERS/HAUSA PROGRAMNERS 
+
+contact: +2348131116661
+
+fb_account: itz real Abdul gee
+
+☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠
+	 (҂`_´)
+         <,︻╦̵̵̿╤─ ҉     ~  • ABDULGEE
+█۞███████]▄▄▄▄▄▄▄▄▄▄▃ ●●●
+▂▄▅█████████▅▄▃▂…
+[███████████████████]
+◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙
+	
+	HACK ANY INSTAGRAM ACCOUNT
+	
+	""")
+	g = input("""
+Target username: """)
+	time.sleep(5)
+	print("CRACKING......please wait......? 10% ")
+	time.sleep(3)
+	print("Hacking......please wait......? 20% ")
+	time.sleep(3)
+	print("Hacking......please wait......? 30% ")
+	time.sleep(3)
+	print("CRACKING......please wait......? 40% ")
+	time.sleep(3)
+	print("Hacking......please wait......? 50% ")
+	time.sleep(3)
+	print("Hacking......please wait......? 60%")
+	time.sleep(3)
+	print("CRACKING......please wait......? 70% ")
+	time.sleep(3)
+	print("Hacking......please wait......? 80% ")
+	time.sleep(3)
+	print("Hacking......please wait......? 90%")
+	time.sleep(3)
+	print("CRACKING......please wait......? 99%")
+	time.sleep(3)
+	print("""
+
+HACKING PASSWORD......please wait......? 100%
+
+""")
+	time.sleep(5)
+	print("""
+	
+SUCCESSFUL HACKED THE PASSWORD
+	
+	""")
+	time.sleep(5)
+	print("username is: ", g)
+	print("password is: ", random.randint(1111111, 9999999999))
+##############IG########
+
+elif a == "3":
+	os.system('cls' if os.name == 'nt' else 'clear')
+	print("""
+	+=========================================+
+|..........   PREMIER HACKERS   ...........|
+              ▇▇▇▇▇▇▇▇▇▇▇▇
+Author: ABDULGEE 
+
+forum: HAUSA HACKERS/HAUSA PROGRAMNERS 
+
+contact: +2348131116661
+
+fb_account: itz real Abdul gee
+
+☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠
+┈┈┈┈┈┈▕▔╲
+┈┈┈┈┈┈┈▏▕
+HACK ANY TIKTOK ACCOUNT
+┈┈┈┈┈┈┈▏▕▂▂▂
+▂▂▂▂▂▂╱┈▕▂▂▂▏
+▉▉▉▉▉┈┈┈▕▂▂▂▏
+▉▉▉▉▉┈┈┈▕▂▂▂▏
+▔▔▔▔▔▔╲▂▕▂▂▂I
+	
+	""")
+	t = input ("target username: ")
+	print("""
+	
+	please wait we are getting the target account.....loading.....loading....loading""")
+	time.sleep(3)
+	print("""
+	loading.....""")
+	time.sleep(3)
+	print("""
+	loading......""")
+	time.sleep(3)
+	print("""
+	loading.......""")
+	time.sleep(4)
+	print("username: ", t)
+	y = "K followers"
+	print("followers: ", random.randint(300, 1000,), y)
+	time.sleep(3)
+	print("""
+CRACKING THE PASSWORD........please wait....	
+	
+	""")
+	time.sleep(10)
+	print("username: ", t)
+	print("password is >> ", random.randint(1111111, 999999999))
+########## tiktok########
+
+elif a == "4":
+	os.system('cls' if os.name == 'nt' else 'clear')
+	print("""+=========================================+
+|..........   PREMIER HACKERS   ...........|
+              ▇▇▇▇▇▇▇▇▇▇▇▇
+Author: ABDULGEE 
+
+forum: HAUSA HACKERS/HAUSA PROGRAMNERS 
+
+contact: +2348131116661
+
+fb_account: itz real Abdul gee
+
+☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠
+█████████
+█▄█████▄█
+█▼▼▼▼▼
+█
+Bypass otp
+█▲▲▲▲▲
+█████████
+ ██ ██Abdulgee
+	
+	
+	""")
+	print("""
+NOTE: target number with country code
+	e.g  +2348131116661 +22651001615 +1205291-8185
+	""")
+	
+	f = input("target phone number: ")
+	print("""
+	wait it can take 1minute..... to get the code
+	
+	""")
+	time.sleep(20)
+	print("number: ", f)
+	time.sleep(4)
+	print("code is: ", random.randint(111001,  999999))
+	print("successful bypassed")
+else:
+		print("check it again")
+elif a == "5":
+	print("""
+	+=========================================+
+|..........   PREMIER HACKERS   ...........|
+              ▇▇▇▇▇▇▇▇▇▇▇▇
+Author: ABDULGEE 
+
+forum: HAUSA HACKERS/HAUSA PROGRAMNERS 
+
+contact: +2348131116661
+
+fb_account: itz real Abdul gee
+
+☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠
+	 (҂`_´)
+         <,︻╦̵̵̿╤─ ҉     ~  • ABDULGEE
+█۞███████]▄▄▄▄▄▄▄▄▄▄▃ ●●●
+▂▄▅█████████▅▄▃▂…
+[███████████████████]
+◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙
+	HACK WHATSAPP BYPASS OTP 
+			""")
+			p = input("target phone number: ")
+			time.sleep(4)
+			print(""" wait..................wait.........wait.........wait.........wait..........wait""")
+			print("""
+			
+			COMING SOOOOOON
+			
+			""")
+			
+			
